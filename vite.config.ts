2866f508a0db3440d38a32839586bf2ff19010ef
Raghuveeedv <153164722+Raghuveeedv@users.noreply.github.com>
2025-06-20 14:03:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173, // Important for Render dev mode
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173, // Important for Render production
   },
 });
